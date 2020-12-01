c20ee064b00748dc93e5a470f1d803f1dd3d190e
taoshouyin <527503855@qq.com>
2020-12-01 20:34:19 +0800

    demo代码提交

---
+++ samples/apiserver_acl_blacklist/blacklistserver01/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/micro-plat/hydra"
+	_ "github.com/micro-plat/hydra/components/caches/cache/redis"
+	"github.com/micro-plat/hydra/conf/server/acl/blacklist"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/hydra/hydra/servers/http"
+)
+
+var app = hydra.NewApp(
+	hydra.WithServerTypes(http.API),
+	hydra.WithPlatName("hydratest"),
+	hydra.WithSystemName("apiserverblacklist"),
+	hydra.WithClusterName("taosytest"),
+	hydra.WithRegistry("zk://192.168.0.101"),
+)
+
+func init() {
+	hydra.Conf.API(":8071").BlackList(blacklist.WithDisable(), blacklist.WithIP("192.168.5.107"))
+	app.API("/hydratest/apiserver/blacklist", funcBlackList)
+}
+
+//apiserver 黑名单中间件配置被禁用demo
+
+//1.1  sudo ./blacklistserver01 conf install -cover
+//1.2 使用 ./blacklistserver01 run
+//1.3 调用接口：http://192.168.5.94:8071/hydratest/apiserver/blacklist 通过机器192.168.5.107访问 可正常返回200/success
+func main() {
+	app.Start()
+}
+
+var funcBlackList func(ctx context.IContext) (r interface{}) = func(ctx context.IContext) (r interface{}) {
+	ctx.Log().Info("apiserver_blacklist 黑名单中间件配置被禁用demo")
+	return "success"
+}
---
+++ samples/apiserver_acl_blacklist/blacklistserver03/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/micro-plat/hydra"
+	_ "github.com/micro-plat/hydra/components/caches/cache/redis"
+	"github.com/micro-plat/hydra/conf/server/acl/blacklist"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/hydra/hydra/servers/http"
+)
+
+var app = hydra.NewApp(
+	hydra.WithServerTypes(http.API),
+	hydra.WithPlatName("hydratest"),
+	hydra.WithSystemName("apiserverblacklist"),
+	hydra.WithClusterName("taosytest"),
+	hydra.WithRegistry("zk://192.168.0.101"),
+)
+
+func init() {
+	hydra.Conf.API(":8071").BlackList(blacklist.WithEnable(), blacklist.WithIP("192.168.5.*", "192.168.*.107", "192.168.**"))
+	app.API("/hydratest/apiserver/blacklist", funcBlackList)
+}
+
+//apiserver 黑名单中间件配置启用*模糊匹配demo
+
+//1.1  sudo ./blacklistserver03 conf install -cover
+//1.2 使用 ./blacklistserver03 run
+//1.3 调用接口：http://192.168.5.94:8071/hydratest/apiserver/blacklist 通过机器192.168.5.107访问 可正常返回403/黑名单限制[%s]不允许访问
+//1.4 调用接口：http://192.168.5.94:8071/hydratest/apiserver/blacklist 通过机器192.168.5.94访问 可正常返回403/黑名单限制[%s]不允许访问
+func main() {
+	app.Start()
+}
+
+var funcBlackList func(ctx context.IContext) (r interface{}) = func(ctx context.IContext) (r interface{}) {
+	ctx.Log().Info("apiserver_blacklist 黑名单中间件配置启用*模糊匹配demo")
+	return "success"
+}
