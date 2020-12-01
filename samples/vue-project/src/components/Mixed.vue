ec358ae1b5aa7be371af405e5dab1399510e0e06
liujinyin <liujy@100bm.cn>
2020-12-01 17:05:24 +0800

    增加webserver 的样例vue 项目

---
+++ samples/vue-project/src/components/Mixed.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+      this is web page 
+      <button @click="queryapi" ><button>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios"
+
+export default {
+  name: 'Mixed',
+  props: {
+    msg: String
+  },
+  methods:{
+    queryapi(){
+      axios.get('/self/api').then(function(res){
+          console.log("apiresponse:",res.data);
+      }).catch(function (error) {
+          console.log(error);
+      });
+    }
+  }
+}
+</script>
+ 
