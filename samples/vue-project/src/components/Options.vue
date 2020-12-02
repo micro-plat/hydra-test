db1273d0744287a0232dd4c205ac15eda7e2da83
liujinyin <liujy@100bm.cn>
2020-12-02 16:06:52 +0800

    调整options 的请求

--- samples/vue-project/src/components/Options.vue
+++ samples/vue-project/src/components/Options.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
       <button @click="clickquery">Options</button>
+      <label>{{requestData}}</label>
   </div>
 </template>
 
@@ -13,11 +14,18 @@ export default {
   props: {
     msg: String
   },
+  data(){
+    return {
+      requestData:""
+    }
+  },
   methods:{
     clickquery(){
     var url= window.globalconfig.api_addr+"/options"
+    var that = this;
     axios.get(url).then(function(res){
       console.log(res);
+      that.requestData = res
     }).catch(function (error) {
         console.log(error);
     });
