2a3976801720b4b58025901e5e926016f6f3a5e9
liujinyin <liujy@100bm.cn>
2020-12-02 11:20:37 +0800

    增加样例注释内容

--- samples/webserver_conf/main.go
+++ samples/webserver_conf/main.go
@@ -6,13 +6,11 @@ import (
 
 	"github.com/micro-plat/hydra"
 	"github.com/micro-plat/hydra/conf/server/static"
-	"github.com/micro-plat/hydra/context"
 	"github.com/micro-plat/hydra/global"
 	"github.com/micro-plat/hydra/hydra/servers/http"
 )
 
 var app = hydra.NewApp(
-	hydra.WithDebug(),
 	hydra.WithServerTypes(http.Web),
 	hydra.WithPlatName("hydratest"),
 	hydra.WithSystemName("webserverconf"),
@@ -22,9 +20,12 @@ var app = hydra.NewApp(
 
 func init() {
 	vueconfig("conf")
-	hydra.Conf.Web(":50003").Static(static.WithArchive("dist.zip"), static.WithRoot("./dist"))
+	hydra.Conf.Web(":50003").Static(static.WithArchive("dist.zip"), static.WithRoot("./"))
 }
 
+//提供api接口，该接口从注册中心拉取配置返回给前端
+//1.启动程序：go run main.go run
+//2.浏览器访问： http://localhost:50003 查看控制台输出服务器配置信息
 func main() {
 	app.Start()
 }
@@ -37,7 +38,7 @@ func vueconfig(cur string) {
 			"currentComponent": cur,
 		},
 	}
-	app.Web("/vue/config", func(ctx context.IContext) interface{} {
+	app.Web("/vue/config", func(ctx hydra.IContext) interface{} {
 		data := map[string]interface{}{}
 		ctx.APPConf().GetVarConf().GetObject("config", "vue", &data)
 		return data
