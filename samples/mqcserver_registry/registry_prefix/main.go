05c3112e7b1c1894e5ed21fa7b19089b01b37702
hj <546921722@qq.com>
2020-12-02 17:58:36 +0800

    mqcsever_registry

---
+++ samples/mqcserver_registry/registry_prefix/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/micro-plat/hydra"
+	"github.com/micro-plat/hydra/conf/vars/queue/queueredis"
+	"github.com/micro-plat/hydra/conf/vars/redis"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/hydra/global"
+	"github.com/micro-plat/hydra/hydra/servers/mqc"
+	"github.com/micro-plat/hydra/services"
+)
+
+var app = hydra.NewApp(
+	hydra.WithServerTypes(mqc.MQC),
+	hydra.WithPlatName("hydra_test"),
+	hydra.WithSystemName("mqc_registry_prefix"),
+	hydra.WithClusterName("t"),
+	hydra.WithRegistry("lm://./"),
+)
+
+func init() {
+	global.MQConf.PlatNameAsPrefix(false) //设置不拼接平台名为前缀
+	hydra.Conf.Vars().Redis("5.79", redis.New([]string{"192.168.5.79:6379"}))
+	hydra.Conf.Vars().Queue().Redis("xxx", queueredis.New(queueredis.WithConfigName("5.79")))
+	hydra.Conf.MQC("redis://xxx")
+	app.MQC("/mqc", func(ctx context.IContext) (r interface{}) {
+		ctx.Log().Info("mqc")
+		return
+	}, "mqcName")
+	app.MQC("/mqc2", func(ctx context.IContext) (r interface{}) {
+		ctx.Log().Info("mqc")
+		return
+	}, "mqcName2")
+}
+
+//测试mqc服务注册时队列名不拼接平台名为前缀
+//启动服务  ./registry_assign run
+//查看的服务启动前的消息队列数量[mqcName,mqcName2]和启动后的消息队列信息
+func main() {
+	fmt.Println("服务启动前的队列")
+	for k, v := range services.MQC.GetQueues().Queues {
+		fmt.Println(k, v.Queue, v.Service, v.Disable, v.Concurrency)
+	}
+	hydra.OnReady(func() {
+		fmt.Println("服务启动后的队列")
+		for k, v := range services.MQC.GetQueues().Queues {
+			fmt.Println(k, v.Queue, v.Service, v.Disable, v.Concurrency)
+		}
+	})
+	app.Start()
+}
---
+++ samples/mqcserver_registry/registry_repeat/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/micro-plat/hydra"
+	"github.com/micro-plat/hydra/conf/vars/queue/queueredis"
+	"github.com/micro-plat/hydra/conf/vars/redis"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/hydra/hydra/servers/mqc"
+	"github.com/micro-plat/hydra/services"
+)
+
+var app = hydra.NewApp(
+	hydra.WithServerTypes(mqc.MQC),
+	hydra.WithPlatName("hydra_test"),
+	hydra.WithSystemName("mqc_registry-repeat"),
+	hydra.WithClusterName("t"),
+	hydra.WithRegistry("lm://./"),
+)
+
+func init() {
+	hydra.Conf.Vars().Redis("5.79", redis.New([]string{"192.168.5.79:6379"}))
+	hydra.Conf.Vars().Queue().Redis("xxx", queueredis.New(queueredis.WithConfigName("5.79")))
+	hydra.Conf.MQC("redis://xxx")
+	app.MQC("/mqc", func(ctx context.IContext) (r interface{}) {
+		ctx.Log().Info("mqc")
+		return
+	})
+}
+
+//测试动态注册与取消（同一对列多次注册与取消)
+//启动服务  ./registry_repeat run
+//查看的服务启动前的消息队列数量[0]和启动后的消息队列信息
+func main() {
+	mqcName := "mqcName"
+	mqcService := "/mqc"
+	hydra.MQC.Add(mqcName, mqcService)
+	hydra.MQC.Remove(mqcName, mqcService)
+	hydra.MQC.Add(mqcName, mqcService)
+	hydra.MQC.Remove(mqcName, mqcService)
+	fmt.Println("服务启动前的队列:", services.MQC.GetQueues().Queues)
+	hydra.OnReady(func() {
+		fmt.Println("服务启动后的队列")
+		for k, v := range services.MQC.GetQueues().Queues {
+			fmt.Println(k, v.Queue, v.Service, v.Disable, v.Concurrency)
+		}
+	})
+	app.Start()
+
+}
