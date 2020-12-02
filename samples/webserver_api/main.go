8fc91bd23bd80da321aa681ff98cb465c4776bd8
liujinyin <liujy@100bm.cn>
2020-12-02 11:15:09 +0800

    调整 context 的引用 ，增加注释

--- samples/webserver_api/main.go
+++ samples/webserver_api/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/micro-plat/hydra"
 	"github.com/micro-plat/hydra/conf/server/static"
-	"github.com/micro-plat/hydra/context"
-	"github.com/micro-plat/hydra/global"
 	"github.com/micro-plat/hydra/hydra/servers/http"
 )
 
 var app = hydra.NewApp(
-	hydra.WithDebug(),
 	hydra.WithServerTypes(http.Web),
 	hydra.WithPlatName("hydratest"),
 	hydra.WithSystemName("webserverapi"),
@@ -23,11 +19,16 @@ var app = hydra.NewApp(
 func init() {
 	vueconfig("mixed")
 	hydra.Conf.Web(":50004").Static(static.WithArchive("dist.zip"), static.WithRoot("./"))
-	app.Web("/self/api", func(ctx context.IContext) interface{} {
+	app.Web("/self/api", func(ctx hydra.IContext) interface{} {
 		return "self.api.success"
 	})
 }
 
+//混合静态文件，api接口是否正常工作
+//启动程序 go run main.go run
+//1. http://localhost:50004 首页正常访问
+//2. 点击页面中的 按钮，请求接口地址 正常返回
+//3. http://localhost:50004/self/api 正常返回
 func main() {
 	app.Start()
 }
@@ -35,12 +36,12 @@ func main() {
 func vueconfig(cur string) {
 	hydra.Conf.Vars()["config"] = map[string]interface{}{
 		"vue": map[string]interface{}{
-			"api_addr":         fmt.Sprintf("//%s:50002", global.LocalIP()),
+			"api_addr":         "",
 			"version":          time.Now().Format("20060102150405"),
 			"currentComponent": cur,
 		},
 	}
-	app.Web("/vue/config", func(ctx context.IContext) interface{} {
+	app.Web("/vue/config", func(ctx hydra.IContext) interface{} {
 		data := map[string]interface{}{}
 		ctx.APPConf().GetVarConf().GetObject("config", "vue", &data)
 		return data
