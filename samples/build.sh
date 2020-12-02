5b8b55fac656c0aaec6fba085de7e32f9bbc9f6b
liujinyin <liujy@100bm.cn>
2020-12-02 12:03:56 +0800

    添加统一的编译与删除命令

---
+++ samples/build.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+BASE_DIR=$(cd -P $(dirname $0);pwd)
+ 
+
+build_go_app() {
+    for file in `ls $1`
+    do
+        if [ "$file" = "vendor" ]
+        then 
+            return
+        fi
+        if [ -d $1"/"$file ]  #注意此处之间一定要加上空格，否则会报错
+        then
+            build_go_app $1"/"$file
+        else
+            if [ "$file" = "main.go" ] 
+            then
+                cd $1
+                echo "go build $1"
+                go build
+                cd $BASE_DIR
+                sleep 0.1
+            fi            
+        fi
+    done
+}
+
+#测试目录 test
+build_go_app ./
+
+
