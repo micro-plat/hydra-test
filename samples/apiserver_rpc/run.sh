34d4dbe57413100b7b6d975bf53cf8528e82ac1e
liujinyin <liujy@100bm.cn>
2020-12-03 11:29:47 +0800

    修改apiserver rpc 转发的样例程序

--- samples/apiserver_rpc/run.sh
+++ samples/apiserver_rpc/run.sh
@@ -7,7 +7,9 @@ go build
 sleep 1
 
 localip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
-echo 浏览器请求：$localip:50008/api/localrpc
+echo 浏览器请求：$localip:50008/api/requestbyctx
+echo 浏览器请求：$localip:50008/api/swap
+echo 浏览器请求：$localip:50008/api/request
 echo 浏览器请求：$localip:50008/api/remoterpc
 echo 浏览器请求：$localip:50008/api/remoterpcip
 
